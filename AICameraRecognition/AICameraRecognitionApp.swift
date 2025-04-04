d35dcd8dbc76476c605cda818511ce03967093d2
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-04-04 16:15:19 +0300

    Initial Commit

---
+++ AICameraRecognition/AICameraRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICameraRecognitionApp.swift
+//  AICameraRecognition
+//
+//  Created by Roman Tverdokhleb on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AICameraRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
