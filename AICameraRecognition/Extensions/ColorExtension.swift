3789669584466f00df45941ff9a3e46630074208
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-04-04 17:13:36 +0300

    new splash screen added

---
+++ AICameraRecognition/Extensions/ColorExtension.swift
@@ -0,0 +1,28 @@
+//
+//  Colors.swift
+//  AICameraRecognition
+//
+//  Created by Roman Tverdokhleb on 4/4/25.
+//
+
+import SwiftUI
+
+extension Color {
+    enum BackColors {
+        static let backElevated = Color("BackElevated")
+        static let backiOSPrimary = Color("BackiOSPrimary")
+        static let backPrimary = Color("BackPrimary")
+        static let backSecondary = Color("BackSecondary")
+        static let backDefault = Color("BackDefault")
+    }
+    
+    enum LabelColors {
+        static let labelDisable = Color("LabelDisable")
+        static let labelDetails = Color("LabelDetails")
+        static let labelPrimary = Color("LabelPrimary")
+        static let labelSecondary = Color("LabelSecondary")
+        static let labelTertiary = Color("LabelTertiary")
+        static let labelReversed = Color("LabelReversed")
+        static let labelBlack = Color("LabelBlack")
+    }
+}
